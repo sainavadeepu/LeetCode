507d7f32a1675fb5a25305f59a9d1de48904f5a4
sainavadeepu <navadeep2266@gmail.com>
2026-05-22 12:39:34 +0530

    Added Rotate Array

---
+++ Arrays/RotateArray(186).cpp
@@ -0,0 +1,30 @@
+/*189. Rotate Array
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
+    }
+};
