f270286e8972b66ff80073d0fe330dfcd406042f
sainavadeepu <navadeep2266@gmail.com>
2026-05-25 22:33:32 +0530

    added Majority element 1

---
+++ Arrays/majorityelement1(169).cpp
@@ -0,0 +1,39 @@
+/*169. Majority Element
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int >hash;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+        for(auto it:hash){
+            if(it.second>(n/2)){
+                return it.first;
+            }
+        }
+        return -1;
+        
+    }
+};
