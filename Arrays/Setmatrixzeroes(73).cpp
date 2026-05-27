d943061846a811da4e88671889de7211f69cee68
sainavadeepu <navadeep2266@gmail.com>
2026-05-27 23:01:13 +0530

    added setmatrixzero

---
+++ Arrays/Setmatrixzeroes(73).cpp
@@ -0,0 +1,56 @@
+/*
+73. Set Matrix Zeroes
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Example 2:
+
+
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    void setZeroes(vector<vector<int>> & matrix){
+        int n=matrix.size();
+        int m=matrix[0].size();
+     
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+        
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+
+                }
+            }
+        }
+    }
+};
+        
+
