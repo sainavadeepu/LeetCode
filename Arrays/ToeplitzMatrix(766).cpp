6956baa4d3577c85b28b6b82c9cc11270dac4ecb
sainavadeepu <navadeep2266@gmail.com>
2026-05-28 17:42:46 +0530

    addtorplitz

--- Arrays/ToeplitzMatrix(766).cpp
+++ Arrays/ToeplitzMatrix(766).cpp
@@ -35,9 +35,9 @@ public:
     bool istoeplitzm(vector<vector<int>>& matrix){
         int r=matrix.size();
         int c=matrix[0].size();
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(matrix[i][j]!=matrix[j][i]){
+        for(int i=0;i<r-1;i++){
+            for(int j=0;j<c-1;j++){
+                if(matrix[i][j]!=matrix[i+1][j+1]){
                     return false;
                 }
             }
