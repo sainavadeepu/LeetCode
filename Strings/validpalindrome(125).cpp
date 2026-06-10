1812d4a2a0d29a3f96ab6dee1fb7f107ddd6e872
sainavadeepu <navadeep2266@gmail.com>
2026-06-10 12:47:41 +0530

    remove all substr occurences

---
+++ Strings/validpalindrome(125).cpp
@@ -0,0 +1,52 @@
+/*
+125. Valid Palindrome
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])){
+                left++;
+            }
+            while(left<right && !isalnum(s[right])){
+                right--;
+            }
+            if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
