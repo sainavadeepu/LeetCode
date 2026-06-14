0a25356e3de72f432da765026632f4d7682927ef
sainavadeepu <navadeep2266@gmail.com>
2026-06-14 16:24:50 +0530

    permutationinastring

---
+++ Strings/permutatoninastring(567).cpp
@@ -0,0 +1,52 @@
+/*
+Code
+Testcase
+Testcase
+Test Result
+567. Permutation in String
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+       int left=0;
+       int right=-1;
+       sort(s1.begin(),s1.end());
+       string ans="";
+       for(char ch:s2){
+        ans+=ch;
+        right++;
+
+        if(right-left+1==s1.size()){
+            string temp=ans.substr(left,right+1);
+            sort(temp.begin(),temp.end());
+            if(temp==s1){
+                return true;
+            }
+        }
+       }
+       return false;
+    }
+};
