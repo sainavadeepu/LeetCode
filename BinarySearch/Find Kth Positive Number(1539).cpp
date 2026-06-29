3b903071a2d69c15c79b2d6dc7ebc32579b84a09
sainavadeepu <navadeep2266@gmail.com>
2026-06-29 19:35:01 +0530

    added Kthpositive Number

---
+++ BinarySearch/Find Kth Positive Number(1539).cpp
@@ -0,0 +1,44 @@
+/*1539. Kth Missing Positive Number
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Return the kth positive integer that is missing from this array.
+
+ 
+
+Example 1:
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low=0, high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+
+        }
+        return k+low;
+    }
+};
