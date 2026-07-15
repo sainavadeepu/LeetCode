48b46f635975f206ab484c8be9881ba3fe23cd5a
sainavadeepu <navadeep2266@gmail.com>
2026-07-15 20:21:58 +0530

    added findallanagrams

---
+++ Sliding Window/Find allargumentsinastring(438).cpp
@@ -0,0 +1,65 @@
+/*
+438. Find All Anagrams in a String
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+ 
+
+Constraints:
+
+1 <= s.length, p.length <= 3 * 104
+s and p consist of lowercase English letters.*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> window(26,0);
+        vector<int> count(26,0);
+        vector<int> ans;
+        int n=s.size();
+
+        for(char ch:p){
+            count[ch-'a']++;
+        }
+        int left=0;
+        for(int right=0;right<n;right++){
+            window[s[right]-'a']++;
+
+            if(right-left+1>p.size()){
+                window[s[left]-'a']--;
+                left++;
+            }
+
+            if(right-left+1==p.size()){
+                if(window==count){
+                    ans.push_back(left);
+                }
+
+            }
+        }
+        return ans;
+        
+    }
+};
