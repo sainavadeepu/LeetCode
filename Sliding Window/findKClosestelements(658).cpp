7b14152addf538a28d5fe3a69b96a221d1068e41
sainavadeepu <navadeep2266@gmail.com>
2026-07-16 19:29:20 +0530

    added kclosestelements

---
+++ Sliding Window/findKClosestelements(658).cpp
@@ -0,0 +1,49 @@
+/*658. Find K Closest Elements
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+
+Output: [1,2,3,4]
+
+Example 2:
+
+Input: arr = [1,1,2,3,4,5], k = 4, x = -1
+
+Output: [1,1,2,3
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left=0;
+        int n=arr.size();
+        int right=n-1;
+        while(right-left+1>k){
+            if(abs(arr[left]-x)>abs(arr[right]-x)){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return vector<int>(arr.begin()+left,arr.begin()+right+1);
+        
+        
+        
+    }
+};
