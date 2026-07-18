9c625f785186476decd4ede9dd6e9bb80def0687
sainavadeepu <navadeep2266@gmail.com>
2026-07-18 19:00:28 +0530

    added MAxNoof vowels in substring

--- Sliding Window/largestharmonicsubsequence(594).cpp
+++ Sliding Window/largestharmonicsubsequence(594).cpp
@@ -42,20 +42,22 @@ Explanation:
 No harmonic subsequence exists*/
 #include<bits/stdc++.h>
 using namespace std;
-class Solution{
-    public:
-    int findlhs(vector<int>& nums){
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
         int left=0;
         int ans=0;
+        int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int right=0;right<nums.size();right++){
-            while(nums[right]-nums[left]>1){
-                left++;
-            }
-            if(nums[right]-nums[left]==1){
-                ans=max(ans,right-left+1);
-            }
+        for(int right=0;right<n;right++){
+        while(nums[right]-nums[left]>1){
+            left++;
+        }
+        if(nums[right]-nums[left]==1){
+            ans=max(ans,right-left+1);
+        }
         }
         return ans;
+        
     }
-}
+};
