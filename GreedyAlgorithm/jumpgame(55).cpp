3ae50e526976c50ef12842026d2f8043f416ea95
sainavadeepu <navadeep2266@gmail.com>
2026-08-31 22:47:29 +0530

    added jumpgame

---
+++ GreedyAlgorithm/jumpgame(55).cpp
@@ -0,0 +1,44 @@
+/*
+55. Jump Game
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>reach){
+                return false;
+            }
+            reach=max(reach,i+nums[i]);
+
+            if(reach>=n-1){
+                return true;
+            }
+        }
+        return true;
+        
+    }
+};
